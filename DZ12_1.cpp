2a3a5a4045b032255b5ccc3b855306c5abdc90f3
ROMANGREKHOV <94071555+ROMANGREKHOV@users.noreply.github.com>
2026-01-05 18:16:17 +0300

    DZ12_1.cpp

--- DZ12_1.cpp
+++ DZ12_1.cpp
@@ -1,17 +1,18 @@
-//#include <iostream>
-//
-//int main()
-//{
-//	std::string stringName{};
-//	std::string stringSurname{};
-//
-//	std::cout << "Введите имя: ";
-//	std::cin >> stringName;
-//	std::cout << "Введите фамилию: ";
-//	std::cin >> stringSurname;
-//
-//	stringName = stringName + ' ' + stringSurname;
-//	std::cout << "Здравствуйте, " << stringName << '!' << "\n\n\n";
-// 
-//	return EXIT_SUCCESS;
-//}
+#include <iostream>
+
+int main()
+{
+	std::string stringName{};
+	std::string stringSurname{};
+
+	std::cout << "Введите имя: ";
+	std::cin >> stringName;
+	std::cout << "Введите фамилию: ";
+	std::cin >> stringSurname;
+
+	stringName = stringName + ' ' + stringSurname;
+	std::cout << "Здравствуйте, " << stringName << '!' << "\n\n\n";
+
+	return EXIT_SUCCESS;
+
+}
